e819a7d9b9a2c3a7bc88f2780a45244e1988b778
Michael Stuart <mikewstuart@gmail.com>
2017-03-22 22:16:07 -0700

    Updates

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,5 +1,41 @@
 window.onload = () => {
 
+
+
+  const plan = JSON.parse(window.localStorage.getItem('_workout_plan'));
+  const twentyFourHours = 1000 * 60 * 60 * 24;
+  const currentUTC = Date.parse(new Date);
+  const days = Object.keys(plan);
+  const NUMBER_OF_WEEKS = 1;
+  const log = [];
+
+  const generateLog = () => {
+    let j = 1
+    for (let i = 1; i <= (NUMBER_OF_WEEKS * 7); i++) {
+      const date = currentUTC - (twentyFourHours * i);
+      const day = days[new Date(date).getDay()]
+      if(plan[day].length > 0) {
+        const workout = {[date]:{"exercises":[]}}
+        plan[day].forEach((exercise, index) => {
+          workout[date].exercises.push({[exercise.name]: []});
+          for(let k = 0; k < 3; k++) {
+            workout[date].exercises[index][exercise.name].push((((NUMBER_OF_WEEKS * 7) + 30) - (Math.floor(Math.random() * 5) + j)) - (k * 5));
+          }
+        })
+        log.push(workout)
+        j++;
+      }
+    }    
+    window.localStorage.setItem('_workout_log', JSON.stringify(log.reverse()));
+    alert('Workout Log Created')
+  }
+
+  document.getElementById('generate').addEventListener('click', () => {
+    generateLog();
+  })
+
+
+
   let alerted;
 
   document.getElementById('new').addEventListener('click', () => {
