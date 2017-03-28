b15780631bc4dba8b30a1831065405e38f80609f
Michael Stuart <mikewstuart@gmail.com>
2017-03-28 16:43:07 -0700

    Updates

--- js/index.js
+++
@@ -1,7 +0,0 @@
-window.onload = () => {
-  if(window.localStorage.getItem('_workout_plan')) {
-    window.location.replace('dashboard.html');
-  } else {
-    window.location.replace('landing.html');
-  }
-}
